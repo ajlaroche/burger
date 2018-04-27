2644e29422bc7d74fc82982e1520ea36c794d3ef
Antony Laroche <antony@larochefamily.net>
2018-04-26 22:46:36 -0500

    initial setup of server

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+var mysql = require("mysql");
+var keys = require("./keys.js");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: keys.mysql.mysql_password,
+    database: "burgers_db"
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id" + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+USE burgers_db;
+
+INSERT INTO burgers (
+burger_name
+, devoured
+)
+VALUES (
+'black bean burger'
+, false
+),
+(
+'bison burger'
+, false
+),
+(
+'turkey burger'
+, false
+);
+
+SELECT * FROM burgers;
